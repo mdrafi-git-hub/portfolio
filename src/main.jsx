3783ead9320e584712b6be8f7836694f59008a2e
mdrafi-git-hub <rafimohammad055@gmail.com>
2025-07-09 00:47:49 +0530

    Ensure Bootstrap is installed and imported correctly

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // ✅ This enables toggles & collapse
-
 
 
 createRoot(document.getElementById('root')).render(
